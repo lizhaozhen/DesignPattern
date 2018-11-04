123973bdeb0f20f4935b2c31f3874014a7ec3efb
new <lilibupt123@gmail.com>
2018-11-04 21:17:49 +1100

    Prototype

---
+++ Lib/ShapesAndColors/ShapeCache.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+namespace Lib.ShapesAndColors
+{
+    public class ShapeCache
+    {
+        private static Dictionary<string, IShape> shapeDic = new Dictionary<string, IShape>();
+
+        public static void LoadCache()
+        {
+            shapeDic["Circle"] = new Circle();
+            shapeDic["Square"] = new Square();
+            shapeDic["Rectangle"] = new Rectangle();
+        }
+
+        public static IShape GetShape(string typeName)
+        {
+            var shape = shapeDic[typeName];
+            return shape;
+        }
+    }
+}
