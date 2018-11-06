1bf8d7665e60fc35397b2ad18dcb907409074306
new <lilibupt123@gmail.com>
2018-11-06 12:39:19 +1100

    Template pattern

---
+++ Lib/TemplatePattern/Football.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Lib.TemplatePattern
+{
+    public class Football : Game
+    {
+        public override void EndPlay()
+        {
+            // end play football
+        }
+
+        public override void Initialize()
+        {
+            // initialize football
+        }
+
+        public override void StartPlay()
+        {
+            // start play football
+        }
+    }
+}
---
+++ Lib/TemplatePattern/Game.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Lib.TemplatePattern
+{
+    public abstract class Game
+    {
+        public abstract void Initialize();
+        public abstract void StartPlay();
+        public abstract void EndPlay();
+        public void Play()
+        {
+            Initialize();
+
+            StartPlay();
+
+            EndPlay();
+        }
+    }
+}
